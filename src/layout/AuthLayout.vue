864d3a5c0e690776477c60a336f27395e23c5b29
AlexMaster168 <alex4185@ukr.net>
2022-01-09 00:34:44 +0200

    "addition authrorization with error and validation"

---
+++ src/layout/AuthLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container with-nav">
+     <app-message/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Auth from "@/views/Auth";
+import AppMessage from "@/components/ui/AppMessage"
+
+export default {
+  name: "AuthLayout",
+  components: { AppMessage, Auth}
+}
+</script>
+
+<style scoped>
+
+</style>
